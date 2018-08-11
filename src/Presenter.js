2efd03d82053405f974ea9a2958cfb8d169693ee
Brent Jackson <jxnblk@gmail.com>
2018-08-11 11:23:59 -0700

    Add new window button in presenter mode

--- src/Presenter.js
+++ src/Presenter.js
@@ -7,6 +7,7 @@ import Slide from './Slide'
 import Root from './Root'
 import Timer from './Timer'
 import Mono from './Mono'
+import Button from './Button'
 
 export const Presenter = ({
   index,
@@ -75,6 +76,14 @@ export const Presenter = ({
       <Flex mt='auto' px={3} py={3}>
         <Mono>Slide {index} of {length}</Mono>
         <Box mx='auto' />
+        <Button
+          onClick={e => {
+            const { pathname } = window.location
+            window.open(pathname, '_blank')
+          }}>
+          New Window
+        </Button>
+        <Box mx={3} />
         <Timer />
       </Flex>
     </Flex>
--- src/Timer.js
+++ src/Timer.js
@@ -1,32 +1,9 @@
 import React from 'react'
 import hhmmss from 'hhmmss'
-import styled from 'styled-components'
-import { space, color } from 'styled-system'
 import Flex from './Flex'
 import Box from './Box'
 import Mono from './Mono'
-
-const Button = styled.button([], {
-  appearance: 'none',
-  fontFamily: 'inherit',
-  fontSize: '12px',
-  fontWeight: 'bold',
-  borderRadius: '4px',
-  border: 'none'
-}, space, color)
-
-Button.propTypes = {
-  ...space.propTypes,
-  ...color.propTypes
-}
-
-Button.defaultProps = {
-  m: 0,
-  px: 2,
-  py: 1,
-  color: 'white',
-  bg: '#333'
-}
+import Button from './Button'
 
 class Timer extends React.Component {
   state = {
